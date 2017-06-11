e0f5923e6f2102963a1a47cf86d72e62a61d2f57
Antonio <berser.ar@gmail.com>
2017-06-11 16:41:58 -0500

    Added settings persistence

--- src/core/store/configureStore.dev.js
+++ src/core/store/configureStore.dev.js
@@ -1,6 +1,7 @@
 import { applyMiddleware, compose, createStore } from 'redux'
 import createSagaMiddleware from 'redux-saga'
 import createLogger from 'redux-logger'
+import persistState from 'redux-localstorage'
 import reducers from '../reducers'
 import sagas from '../sagas'
 
@@ -10,14 +11,9 @@ const logger = createLogger({
 
 export default function configureStore () {
   const sagaMiddleware = createSagaMiddleware()
-  let middleware = applyMiddleware(sagaMiddleware, logger)
-
-  const devToolsExtension = window.devToolsExtension
-  if (typeof devToolsExtension === 'function') {
-    middleware = compose(middleware, devToolsExtension())
-  }
-
-  const store = createStore(reducers, middleware)
+  const middleware = applyMiddleware(sagaMiddleware, logger)
+  const enhancer = compose(middleware, persistState('settings'))
+  const store = createStore(reducers, enhancer)
   sagaMiddleware.run(sagas)
 
   if (module.hot) {
